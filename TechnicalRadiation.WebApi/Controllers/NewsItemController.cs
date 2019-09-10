7b49b06f8158c3c8744cec0b03743a253a757cf0
Grétar Hjartarson <gretarh17@ru.is>
2019-09-10 16:31:28 +0000

    óklárað controller

---
+++ TechnicalRadiation.WebApi/Controllers/NewsItemController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TechnicalRadiation.Models;
+using TechnicalRadiation.Models.Dto;
+using TechnicalRadiation.Services;
+
+namespace TechnicalRadiation.WebApi.Controllers
+{
+    [Route("api/")]
+    public class NewsItemController : Controller
+    {
+        private NewsItemService _newsItemService;
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetAllNewsItems([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 25)
+        {
+            
+            var envelope = new Envelope<NewsItemDto>(pageNumber, pageSize, listi);
+            return Ok(envelope);
+        }
+    }
+}
